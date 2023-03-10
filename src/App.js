619cb306d83798ac1e0fc9b4af0c81390d4f4cdd
Akhad <abdurazakovahad@gmail.com>
2023-03-10 14:02:37 +0300

    formatted the code on ESLint

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Users from "./components/users";
 import api from "./api";
 
 function App() {
-  const [users, setUsers] = useState(api.users.fetchAll());
+    const [users, setUsers] = useState(api.users.fetchAll());
 
-  const handleDelete = (userId) => {
-    const userData = users.filter((user) => user._id !== userId);
-    setUsers(userData);
-  };
+    const handleDelete = (userId) => {
+        const userData = users.filter((user) => user._id !== userId);
+        setUsers(userData);
+    };
 
-  const handleToggleBookmark = (id) => {
-    const elementIndex = users.findIndex((elem) => elem._id === id);
-    const newBookmark = [...users];
-    newBookmark[elementIndex].bookmark = !newBookmark[elementIndex].bookmark;
-    setUsers(newBookmark);
-  };
+    const handleToggleBookmark = (id) => {
+        const elementIndex = users.findIndex((elem) => elem._id === id);
+        const newBookmark = [...users];
+        newBookmark[elementIndex].bookmark =
+            !newBookmark[elementIndex].bookmark;
+        setUsers(newBookmark);
+    };
 
-  return (
-    <div className="App">
-      <SearchStatus length={users.length} />
-      <Users
-        onToggleBookmark={handleToggleBookmark}
-        users={users}
-        onDelete={handleDelete}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <SearchStatus length={users.length} />
+            <Users
+                onToggleBookmark={handleToggleBookmark}
+                users={users}
+                onDelete={handleDelete}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/qualitie.jsx
+++ src/components/qualitie.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Qualitie = ({ name, color }) => {
-  return <span className={`badge bg-${color} m-1`}>{name}</span>;
+    return <span className={`badge bg-${color} m-1`}>{name}</span>;
 };
 
 export default Qualitie;
