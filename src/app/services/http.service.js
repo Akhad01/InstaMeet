b74ef1862b7f368ce99622b9194690673321edc4
Akhad <abdurazakovahad@gmail.com>
2023-06-04 16:14:30 +0300

    Added Refresh tokens

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 
 import configFile from '../config.json'
+import { httpAuth } from '../../hooks/useAuth'
+import localStorageService from './localStorage.service'
 
 const http = axios.create({
   baseURL: configFile.apiEndpoint,
@@ -10,12 +12,29 @@ const http = axios.create({
 http.defaults.baseURL = configFile.apiEndpoint
 
 http.interceptors.request.use(
-  (config) => {
+  async function (config) {
     if (configFile.isFireBase) {
       const containSlash = /\/$/.test(config.url)
 
       config.url =
         (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
+
+      const expiresDate = localStorageService.getTokenDate()
+      const refreshToken = localStorageService.getRefreshToken()
+
+      if (refreshToken && expiresDate < Date.now()) {
+        const { data } = await httpAuth.post('token', {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+        })
+
+        localStorageService.setToken({
+          refreshToken: data.refresh_token,
+          idToken: data.id_token,
+          localId: data.user_id,
+          expiresIn: data.expires_in,
+        })
+      }
     }
 
     return config
